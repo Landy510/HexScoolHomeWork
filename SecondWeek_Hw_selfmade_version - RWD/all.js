a4a1549e7e5dfdd41912700bb9fc97a44d355f9b
Landy510 <randychen510@gmail.com>
2020-12-22 11:48:05 +0800

    新增體驗營第三周作業的RWD版本

---
+++ SecondWeek_Hw_selfmade_version - RWD/all.js
@@ -0,0 +1,8 @@
+let rwd_nav_option_el = document.querySelector(".rwd-nav-option");
+document.querySelector('.rwd-hamburger-btn').addEventListener('click', function(){
+    if(document.body.className === 'show-menu'){
+        document.body.className = '';
+    } else {
+        document.body.className = 'show-menu';
+    }
+}, false);
