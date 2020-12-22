1652535a108bf78f9a0cab9684a9114a7e98619e
Landy510 <randychen510@gmail.com>
2020-12-22 16:12:04 +0800

    新增將Aos元件和Animate.css的效果加入rwd的版本

---
+++ SecondWeek_Hw_selfmade_version - RWD - 01/all.js
@@ -0,0 +1,9 @@
+let rwd_nav_option_el = document.querySelector(".rwd-nav-option");
+document.querySelector('.rwd-hamburger-btn').addEventListener('click', function(e){
+    e.preventDefault();
+    if(document.body.className === 'show-menu'){
+        document.body.className = '';
+    } else {
+        document.body.className = 'show-menu';
+    }
+}, false);
