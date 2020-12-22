65eb359f1bacb66823e0b550c4a4ed7e590d018e
Landy510 <randychen510@gmail.com>
2020-12-22 14:04:14 +0800

    修正第三周RWD版型中的漢堡選單按鈕

--- SecondWeek_Hw_selfmade_version - RWD/all.js
+++ SecondWeek_Hw_selfmade_version - RWD/all.js
@@ -1,5 +1,6 @@
 let rwd_nav_option_el = document.querySelector(".rwd-nav-option");
-document.querySelector('.rwd-hamburger-btn').addEventListener('click', function(){
+document.querySelector('.rwd-hamburger-btn').addEventListener('click', function(e){
+    e.preventDefault();
     if(document.body.className === 'show-menu'){
         document.body.className = '';
     } else {
